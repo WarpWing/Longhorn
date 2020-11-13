973966df39b9b9e6504c015661ab8a834c97acb2
Ty Chermsirivatana <professornitro@gmail.com>
2020-11-12 19:40:27 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,8 +18,8 @@ serverstat = [
 def home():
     return '''<p>Welcome to my prototype API Page </p>
 <p>There shouldn't be much to the frontend other than a few plain words. I would say that this should concern the backend more.</p>
+<p>This API is now backed by a full CI/CD Pipeline! Check out the status <a href="https://github.com/WarpWing/Longhorn/actions" target="_blank" rel="noopener">here</a></p> </p>
 <p>&nbsp;</p>
-<p>This API is now backed by a full CI/CD Pipeline! Look at the Pipeline in action <a href="https://github.com/WarpWing/Longhorn/actions" target="_blank" rel="noopener"here</a></p>
 <p>This page was made by <a href="https://github.com/WarpWing/Longhorn" target="_blank" rel="noopener">WarpWing</a></p> 
 '''
 
