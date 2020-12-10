5bffd2002a77d898810d3d54ab06eae2a89c023b
WarpWing <professornitro@gmail.com>
2020-12-10 02:32:26 +0000

    Added to main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def home():
     return '''<p>Welcome to my prototype API Page </p>
 <title> Longhorn API Test </title>
 <p>There shouldn't be much to the frontend other than a few plain words. I would say that this should concern the backend more.</p>
-<p>This API is now backed by a full CI/CD Pipeline! Check out the status of the pipeline <a href="https://github.com/WarpWing/Longhorn/actions" target="_blank" rel="noopener">here</a></p> </p>
+<p>This API is now backed by a full CI/CD Pipeline using Docker and Github Actions! Check out the status of the pipeline <a href="https://github.com/WarpWing/Longhorn/actions" target="_blank" rel="noopener">here</a></p> </p>
 <p>Check this API Endpoint for more information: /api/v1/</p>
 <p>&nbsp;</p>
 <p>This page was made by <a href="https://github.com/WarpWing/Longhorn" target="_blank" rel="noopener">WarpWing</a></p> 
