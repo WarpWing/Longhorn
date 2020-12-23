faebff191f079b52b2b2eef024729109331b4ae2
WarpWing <professornitro@gmail.com>
2020-12-23 13:52:41 +0000

    Added prototype unit tests

--- tests/main_test.py
+++ tests/main_test.py
@@ -1,11 +1,20 @@
 import unittest
 import requests
 
-res = requests.get('http://0.0.0.0:5000')
+app = 'http://0.0.0.0:5000'
+
+r = requests.get(app)
 
 class MyTest(unittest.TestCase):
-    def test(self):
-        self.assertEqual(res.status_code, 200)
+    def test_main(self):
+        self.assertEqual(r.status_code, 200) 
+
+    def test_apiv1(self):
+        self.assertEqual(r.status_code, 200) 
+
+    def test_kfc(self):
+        self.assertEqual(r.status_code, 200)
+        #self.assertEqual(r.headers['content-type'], 'application/json')
 
 if __name__ == "__main__":
     unittest.main()
