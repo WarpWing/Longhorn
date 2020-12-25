33e7cb29f337b15772df45c92d31c561ab9ce4d5
WarpWing <professornitro@gmail.com>
2020-12-25 19:22:06 +0000

    Added Docker-compose and requirements addition for redis

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from typing import Optional
-
 from fastapi import FastAPI
+import redis 
+import time
 
 app = FastAPI()
+cache = redis.Redis(host='redis', port=6379)
 
+def get_hit_count():
+    retries = 5
+    while True:
+        try:
+            return cache.incr('hits')
+        except redis.exceptions.ConnectionError as exc:
+            if retries == 0:
+                raise exc
+            retries -= 1
+            time.sleep(0.5)
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get('/')
+def hello():
+    count = get_hit_count()
+    return 'Hello World! I have been seen {} times.\n'.format(count)
+
 
 
