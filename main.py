1d1fc5808950d87344cfff9f535ad5db6c306b94
WarpWing <professornitro@gmail.com>
2020-12-30 20:38:56 +0000

    Added initContainer basics

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ import aiofiles
 # Use this command to start the application: uvicorn main:app --host 0.0.0.0 --port 5050
 # Misc array of Variables and Class Instances
 app = FastAPI()
-cache = redis.Redis(host='0.0.0.0', port=6379)
+cache = redis.Redis(host='0.0.0.0', port=6300)
 templates = Jinja2Templates(directory='static/')
 
 def get_hit_count(): # Hit Counter for Visit. Stored in Redis. Should Revamp Hit-Counter as Retries should be exponential.
@@ -40,7 +40,7 @@ def hello():
 
 @app.get('/kfc') # I need to figure out how to get JSON into Redis. I don't understand the Redis Docs but we learn somehome :)
 def kfc():
-    kfc = {"Salt" : "2/3 Tablespoon","Thyme" : "1/2 Tablespoon","Basil" :  "1/2 Tablespoon","Oregano" : "1/3 Tablespoon","Celery Salt" : "1/3 Tablespoon","Black Pepper": "1 Tablespoon","Dry Mustard" : "1 Tablespoon","Paprika" : "3 Tablespoon","Garlic Salt" : "2 Tablespoon","Ground Ginger" : "1 Tablespoon","White Pepper" : "3 Tablespoon","MSG" : "1 Teaspoon"}
+    #kfc = {"Salt" : "2/3 Tablespoon","Thyme" : "1/2 Tablespoon","Basil" :  "1/2 Tablespoon","Oregano" : "1/3 Tablespoon","Celery Salt" : "1/3 Tablespoon","Black Pepper": "1 Tablespoon","Dry Mustard" : "1 Tablespoon","Paprika" : "3 Tablespoon","Garlic Salt" : "2 Tablespoon","Ground Ginger" : "1 Tablespoon","White Pepper" : "3 Tablespoon","MSG" : "1 Teaspoon"}
     return kfc
 
 
